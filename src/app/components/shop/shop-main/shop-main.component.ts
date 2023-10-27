ba26d6ea0d30537bb3464aafa0dde896baa1586b
maru33luc <maru33luc@gmail.com>
2023-10-26 22:41:59 -0300

    Collection cargada desde json-server + paginacion

--- src/app/components/shop/shop-main/shop-main.component.ts
+++ src/app/components/shop/shop-main/shop-main.component.ts
@@ -1,104 +1,55 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Funko } from 'src/app/interfaces/interface';
+import { FunkosService } from 'src/app/services/funkos.service';
 
 @Component({
   selector: 'app-shop-main',
   templateUrl: './shop-main.component.html',
   styleUrls: ['./shop-main.component.css']
 })
-export class ShopMainComponent implements OnInit, AfterViewInit {
+export class ShopMainComponent implements OnInit {
+
+  lista: Funko[] = [];
   itemsPerPage = 9;
-  pageLinks: NodeListOf<Element> | undefined;
-  previousPage: HTMLElement = {} as HTMLElement;
-  nextPage: HTMLElement = {} as HTMLElement;
-
-  allItems!: NodeListOf<Element>;
-  totalItems!: number;
-
   currentPage = 0;
+  pages: number[] = [];
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-    this.previousPage = {} as HTMLElement;
-    this.nextPage = {} as HTMLElement;
-  }
+  constructor(private funkoService: FunkosService) {}
 
   ngOnInit() {
-    // Initialize your data, for example, fetch items and set totalItems
-    this.pageLinks = this.elementRef.nativeElement.querySelectorAll('.page-link');
-    this.previousPage = this.elementRef.nativeElement.querySelector('#previous-page');
-    this.nextPage = this.elementRef.nativeElement.querySelector('#next-page');
+    this.mostrarFunkos();
   }
 
-  ngAfterViewInit() {
-    this.allItems = this.elementRef.nativeElement.querySelectorAll('.card-item');
-    this.totalItems = this.allItems.length;
-    this.showPage(this.currentPage);
-
-    if (this.pageLinks) {
-      this.pageLinks.forEach((link, index) => {
-        this.renderer.listen(link, 'click', (event) => {
-          event.preventDefault();
-          if (this.goToPage) {
-            this.goToPage(index);
-          }
-        });
-      });
-    }
-
-    if (this.previousPage) {
-      this.renderer.listen(this.previousPage, 'click', (event) => {
-        event.preventDefault();
-        if (this.goToPage) {
-          this.goToPage(this.currentPage - 1);
-        }
-      });
-    }
-
-    if (this.nextPage) {
-      this.renderer.listen(this.nextPage, 'click', (event) => {
-        event.preventDefault();
-        if (this.goToPage) {
-          this.goToPage(this.currentPage + 1);
-        }
-      });
-    }
-  }
-
-  showPage(pageNumber: number) {
-    const startIndex = pageNumber * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-
-    this.allItems.forEach((item, index) => {
-      if (index >= startIndex && index < endIndex) {
-        this.renderer.setStyle(item, 'position', 'relative');
-        this.renderer.setStyle(item, 'display', 'block');
-      } else {
-        this.renderer.setStyle(item, 'position', 'relative');
-        this.renderer.setStyle(item, 'display', 'none');
-      }
-    });
-
-    if(this.pageLinks){
-       this.pageLinks.forEach((link, index) => {
-      if (index === pageNumber) {
-        this.renderer.addClass(link, 'active');
-      } else {
-        this.renderer.removeClass(link, 'active');
-      }}
-      );
-    
-    }
-    }
-   
-
-  goToPage(pageNumber: number) {
-    if (pageNumber < 0) {
-      this.currentPage = 0;
-    } else if (pageNumber >= Math.ceil(this.totalItems / this.itemsPerPage)) {
-      this.currentPage = Math.ceil(this.totalItems / this.itemsPerPage) - 1;
+  async mostrarFunkos() {
+    const response = await this.funkoService.getFunkos();
+    if (response != undefined) {
+      this.lista = response as Funko[];
+      this.calculateTotalPages();
     } else {
-      this.currentPage = pageNumber;
+      console.log('Error al mostrar los funkos');
     }
-
-    this.showPage(this.currentPage);
   }
+
+  calculateTotalPages() {
+    this.pages = Array(Math.ceil(this.lista.length / this.itemsPerPage)).fill(0).map((_, i) => i);
+  }
+
+  get paginatedItems() {
+    const startIndex = this.currentPage * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.lista.slice(startIndex, endIndex);
+  }
+
+  changePage(newPage: number) {
+    if (newPage < 0) {
+      this.currentPage = 0;
+    } else if (newPage >= this.pages.length) {
+      this.currentPage = this.pages.length - 1;
+    } else {
+      this.currentPage = newPage;
+    }
+    // Prevent the default behavior of the anchor tags
+    return false;
+  }
+  
 }
--- src/app/components/shop/slider-glide/slider-glide.component.ts
+++ src/app/components/shop/slider-glide/slider-glide.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Glide from '@glidejs/glide';
+import { Funko } from 'src/app/interfaces/interface';
+import { FunkosService } from 'src/app/services/funkos.service';
 
 @Component({
   selector: 'app-slider-glide',
@@ -7,5 +9,22 @@ import Glide from '@glidejs/glide';
   styleUrls: ['./slider-glide.component.css']
 })
 export class SliderGlideComponent {
- 
+
+lista: Funko [] = [];	  
+
+constructor(private funkoService: FunkosService) {}
+
+ngOnInit(): void {
+  this.mostrarFunkos();
+}
+
+async mostrarFunkos() {
+  const response = await this.funkoService.getFunkos();
+  if(response!=undefined){
+    this.lista = response as Funko[];
+    console.log(this.lista);
+  }else{
+    console.log('Error al mostrar los funkos');
+  }
+}
 }
